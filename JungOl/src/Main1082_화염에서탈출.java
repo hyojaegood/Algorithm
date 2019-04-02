410420a1b85a44cd31d7162d62e68cfef5152281
hyojaegood <wngywo12@gmail.com>
2019-04-02 22:50:59 +0900

    JungOl 화염에서탈출

---
+++ JungOl/src/Main1082_화염에서탈출.java
@@ -0,0 +1,115 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main1082_화염에서탈출 {
+	static int R, C;
+	static int homeR,homeC,fireR,fireC,startR,startC;
+	static char[][] map = new char[50][50];
+	static int[] dr = {-1,1,0,0};
+	static int[] dc = {0,0,-1,1};
+	static int t;
+	static boolean flag;
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		String input;
+		st = new StringTokenizer(br.readLine());
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		t = 0;
+		for(int i=0;i<R;i++) {
+			input = br.readLine();
+			for(int j=0;j<C;j++) {
+				map[i][j] = input.charAt(j);
+				if(map[i][j]=='S') {
+					startR = i;
+					startC = j;
+				}else if(map[i][j]=='D') {
+					homeR = i;
+					homeC = j;
+				}
+			}
+			
+		}
+		bfs();
+		if(!flag) {
+			bw.write(""+t);
+		}else {
+			bw.write("impossible\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+	public static void bfs() {
+		Queue<location> qFire = new LinkedList<location>();
+		Queue<location> qHuman = new LinkedList<location>();
+		for(int i=0;i<R;i++) {
+			for(int j=0;j<C;j++) {
+				if(map[i][j]=='*') {
+					qFire.add(new location(i,j));			
+				}
+			}
+		}
+		qHuman.add(new location(startR,startC));
+		int tmpR, tmpC;
+		flag = true;
+		while(flag && !qHuman.isEmpty()) {
+			t++;
+			int qSize1 = qFire.size();
+			for(int i=0;i<qSize1;i++) {
+				for(int j=0;j<4;j++) {
+					tmpR = qFire.peek().r + dr[j];
+					tmpC = qFire.peek().c + dc[j];
+					if(tmpR<R && tmpR>=0 && tmpC<C && tmpC>=0) {
+						if(map[tmpR][tmpC]=='.') {
+							map[tmpR][tmpC] = '*';
+							qFire.add(new location(tmpR, tmpC));
+						}
+					}
+				}
+				qFire.poll();
+			}
+			int qSize2 = qHuman.size();
+			for(int i=0;i<qSize2;i++) {
+				for(int j=0;j<4;j++) {
+					tmpR = qHuman.peek().r + dr[j];
+					tmpC = qHuman.peek().c + dc[j];
+					if(tmpR<R && tmpR>=0 && tmpC<C && tmpC>=0) {
+						if(map[tmpR][tmpC]=='.') {
+							map[tmpR][tmpC] = 'S';
+							qHuman.add(new location(tmpR, tmpC));
+						}
+						if(map[tmpR][tmpC]=='D') {
+							flag = false;
+						}
+					}
+				}
+				qHuman.poll();
+			}
+			System.out.printf("%d분후\n",t);
+			for(int i=0;i<R;i++) {
+				for(int j=0;j<C;j++) {
+					System.out.printf("%c",map[i][j]);
+				}System.out.println();
+			}
+		}
+		//불이동
+		//용자이동
+	}
+}
+class location{
+	int r;
+	int c;
+	location(int row, int col){
+		r = row;
+		c = col;
+	}
+}
--- SWEA/src/Solution4340_파이프연결.java
+++ SWEA/src/Solution4340_파이프연결.java
@@ -1,9 +1,4 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Solution4340_파이프연결 {
 	static int N;
@@ -13,26 +8,127 @@ public class Solution4340_파이프연결 {
 	static int minDis;
 	static boolean[][] visit = new boolean[50][50];
 	static int[][] result = new int[1000][2];
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args){
 		// TODO Auto-generated method stub
 		int T;
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		T = Integer.parseInt(br.readLine());
-		StringTokenizer st;
+		Scanner sc = new Scanner(System.in);
+		T = sc.nextInt();
 		for(int tc=1;tc<=T;tc++) {
-			N = Integer.parseInt(br.readLine());
+			N = sc.nextInt();
 			for(int i=0;i<N;i++) {
-				st = new StringTokenizer(br.readLine());
 				for(int j=0;j<N;j++) {
-					map[i][j] = Integer.parseInt(st.nextToken());
+					map[i][j] = sc.nextInt();
 				}
 			}
-			
+			minDis = N*N;
+			visit[0][0] = true;
+			move(0,0,3,1);
+			visit[0][0] = false;
+			System.out.printf("#%d %d\n",tc, minDis);
 		}
 	}
+	public static boolean check(int row, int col, int dir) {
+		if(visit[row][col])return false;
+		if(map[row][col]==0)return false;
+		if(map[row][col]==1) {
+			if(dir!=2 && dir!=3)return false;
+		}else if(map[row][col]==2) {
+			if(dir!=0 && dir!=1)return false;
+		}else if(map[row][col]==3) {
+			if(dir!=2 && dir!=0)return false;
+		}else if(map[row][col]==4) {
+			if((dir!=3 && dir!=0))return false;
+		}else if(map[row][col]==5) {
+			if(dir!=3 && dir!=1)return false;
+		}else if(map[row][col]==6) {
+			if(dir!=1 && dir!=2)return false;
+		} 
+		return true;
+	}
+	public static int changeDir(int row, int col, int dir) {
+		if(map[row][col]==1) {
+			dir = dir;
+		}else if(map[row][col]==2) {
+			dir = dir;
+		}else if(map[row][col]==3) {
+			if(dir==0)dir = 3;
+			else dir =1;
+		}else if(map[row][col]==4) {
+			if(dir==3)dir = 1;
+			else dir =2;
+		}else if(map[row][col]==5) {
+			if(dir==3)dir = 0;
+			else dir =2;
+		}else if(map[row][col]==6) {
+			if(dir==1)dir = 3;
+			else dir =0;
+		}
+		return dir;
+	}
 	public static void move(int row, int col, int dir, int depth) {
-
+		int nextR, nextC;
+		if(depth>minDis)return;
+//		result[depth][0] = row;
+//		result[depth][1] = col;
+		if(row==N-1 && col==N-1) {
+			if(minDis>depth) {
+				minDis=depth;
+			}
+			return;
+		}
+		dir = changeDir(row, col, dir);
+		//다음 방문할 파이프의 좌표
+		nextR = row+dr[dir];
+		nextC = col+dc[dir];
+		if(nextR>=0 && nextR<N && nextC<N && nextC>=0) {
+			int pipe = map[nextR][nextC];
+			if(nextR == N-1 && nextC == N-1) {
+				if(check(nextR,nextC,dir)) {
+					visit[nextR][nextC] = true;
+					move(nextR,nextC,dir,depth+1);
+					visit[nextR][nextC] = false;
+				}
+			}else if(map[nextR][nextC]==1 || map[nextR][nextC] == 2) {
+				map[nextR][nextC] = 1;
+				if(check(nextR,nextC,dir)) {
+					visit[nextR][nextC] = true;
+					move(nextR,nextC,dir,depth+1);
+					visit[nextR][nextC] = false;
+				}
+				map[nextR][nextC] = 2;
+				if(check(nextR,nextC,dir)) {
+					visit[nextR][nextC] = true;
+					move(nextR,nextC,dir,depth+1);
+					visit[nextR][nextC] = false;
+				}
+			}else if(map[nextR][nextC]==3 || map[nextR][nextC]==4 || map[nextR][nextC]==5 ||map[nextR][nextC]==6) {
+				map[nextR][nextC] = 3;
+				if(check(nextR,nextC,dir)) {
+					visit[nextR][nextC] = true;
+					move(nextR,nextC,dir,depth+1);
+					visit[nextR][nextC] = false;
+				}
+				map[nextR][nextC] = 4;
+				if(check(nextR,nextC,dir)) {
+					visit[nextR][nextC] = true;
+					move(nextR,nextC,dir,depth+1);
+					visit[nextR][nextC] = false;
+				}
+				map[nextR][nextC] = 5;
+				if(check(nextR,nextC,dir)) {
+					visit[nextR][nextC] = true;
+					move(nextR,nextC,dir,depth+1);
+					visit[nextR][nextC] = false;
+				}
+				map[nextR][nextC] = 6;
+				if(check(nextR,nextC,dir)) {
+					visit[nextR][nextC] = true;
+					move(nextR,nextC,dir,depth+1);
+					visit[nextR][nextC] = false;
+				}
+			}
+			map[nextR][nextC] = pipe;
+		}
 	}
 	
 }
